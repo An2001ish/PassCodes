244f17448e8cd90954cc8833d90a203f83007503
Anish Baraily <An2001.baraily@gmail.com>
2024-08-30 21:09:42 +0545

    body

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,15 +3,23 @@ import logo from "./logo.png";
 
 const Navbar = () => {
   return (
-    <div className="container flex flex-row">
-      <div className="logo">
-        <img src={logo} alt="" />
+    <div className="container flex flex-row justify-between items-center bg-gray-200 absolute mx-auto max-w-full">
+      <div className="logo flex flex-row justify-center items-center relative w-1/4">
+        <img className="w-14 border-2 border-solid" src={logo} alt="" />
+        <span>PassCodes</span>
       </div>
-      <div className="home">Home</div>
-      <div className="about">About</div>
-      <div className="contact">Contact</div>
+      <ul className="flex flex-row relative w-1/4 justify-evenly">
+        <li>
+          <a href="">Home</a>
+        </li>
+        <li>
+          <a href="">About</a>
+        </li>
+        <li>
+          <a href="">Contact</a>
+        </li>
+      </ul>
     </div>
   );
 };
-
 export default Navbar;
