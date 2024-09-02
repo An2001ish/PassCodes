d978ccc757c6d340189b0bf14175b73b3942d51b
Anish Baraily <An2001.baraily@gmail.com>
2024-09-02 14:36:38 +0545

    working on ui

--- src/components/Manager.jsx
+++ src/components/Manager.jsx
@@ -1,20 +1,59 @@
-import React from 'react'
+import React from "react";
+import logo from "./logo.png";
 
 const Manager = () => {
   return (
     <div>
-     <div class="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
+      <div class="absolute top-0 z-[-2] h-screen w-screen bg-[#000000] bg-[radial-gradient(#ffffff33_1px,#00091d_1px)] bg-[size:20px_20px]"></div>
 
-    <div className="container">
-        <input type="text" />
-        <div className="container2">
-            <input type="text" />
-            <input type="text" />
+      <div className="flex flex-col h-[90vh] justify-center items-center">
+        <div className="flex justify-center items-center absolute top-32 bg-[#000000] bg-[radial-gradient(#ffffff33_1px,#00091d_1px)] bg-[size:20px_20px] text-white text-xl font-bold">
+          <span className="text-white text-4xl">&#91;</span>
+          <img className="w-14" src={logo} alt="" />
+          Pass<span className="text-[#cc4b85] mr-2">Codes</span>
+          <span className="text-white text-4xl">&#93;</span>
+
         </div>
-    </div>
+        <div className="flex flex-col justify-center items-center h-[60vh] w-[60vw] border-2 mx-auto rounded-lg">
+          <input type="text" placeholder="Enter Website URL" />
+          <div className="flex gap-10">
+            <input type="text" placeholder="Enter Username" />
+            <input type="text" placeholder="Enter Password" />
+          </div>
+          <button className="bg-white">Add</button>
+        </div>
+      </div>
 
-    </div>
-  )
-}
+      {/* <div className="flex flex-col h-[10vh]"></div> */}
 
-export default Manager
+      {/* <div className=" mx-auto my-[30vh] max-w-2xl border-2">
+        <div className=" max-w-full flex flex-col border-2 border-red-300 gap-2">
+
+          <input
+            className="field bg-red-200"
+            type="text"
+            placeholder="Enter Website URL"
+          />
+          <div className="flex flex-row w-1/2 justify-between">
+            <input
+              className="bg-green-200"
+              type="text"
+              placeholder="Enter Username"
+            />
+            <input
+              className="bg-blue-200"
+              type="text"
+              placeholder="Enter Password"
+            />
+          </div>
+        </div>
+        <div className="buttons">
+          <button className="bg-white">Add</button>
+        </div>
+      
+      </div> */}
+    </div>
+  );
+};
+
+export default Manager;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,12 +3,14 @@ import logo from "./logo.png";
 
 const Navbar = () => {
   return (
-    <div className="container flex flex-row justify-between items-center bg-gray-200 absolute mx-auto max-w-full">
-      <div className="logo flex flex-row justify-center items-center relative w-1/4">
-        <img className="w-14 border-2 border-solid" src={logo} alt="" />
-        <span>PassCodes</span>
+    <div className="container flex flex-row justify-between items-center bg-gray-200 mx-auto max-w-full">
+      <div className="logo flex flex-row justify-center items-center w-1/4 text-lg font-bold ">
+      <span className="text-[#010c29] text-4xl">&#91;</span>
+          <img className="w-14" src={logo} alt="" />
+          Pass<span className="text-[#cc4b85] mr-2 ml-">Codes</span>
+          <span className="text-[#010c29] text-4xl">&#93;</span>
       </div>
-      <ul className="flex flex-row relative w-1/4 justify-evenly">
+      <ul className="flex flex-row w-1/4 justify-evenly">
         <li>
           <a href="">Home</a>
         </li>
