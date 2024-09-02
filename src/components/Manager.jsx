dba5118e35561ae22fb1b4e02e7cc4749205e87d
Anish Baraily <An2001.baraily@gmail.com>
2024-09-02 18:38:52 +0545

    icons adding

--- src/components/Manager.jsx
+++ src/components/Manager.jsx
@@ -7,51 +7,25 @@ const Manager = () => {
       <div class="absolute top-0 z-[-2] h-screen w-screen bg-[#000000] bg-[radial-gradient(#ffffff33_1px,#00091d_1px)] bg-[size:20px_20px]"></div>
 
       <div className="flex flex-col h-[90vh] justify-center items-center">
-        <div className="flex justify-center items-center absolute top-32 bg-[#000000] bg-[radial-gradient(#ffffff33_1px,#00091d_1px)] bg-[size:20px_20px] text-white text-xl font-bold">
+        <div className="flex justify-center items-center absolute top-[122px] bg-[#000000] bg-[radial-gradient(#ffffff33_1px,#00091d_1px)] bg-[size:20px_20px] text-white text-xl font-bold">
           <span className="text-white text-4xl">&#91;</span>
           <img className="w-14" src={logo} alt="" />
           Pass<span className="text-[#cc4b85] mr-2">Codes</span>
           <span className="text-white text-4xl">&#93;</span>
-
+          <span className="flex absolute text-white font-light w-screen top-16 justify-center text-lg">
+            A Safe Place for Your Passwords
+          </span>
         </div>
-        <div className="flex flex-col justify-center items-center h-[60vh] w-[60vw] border-2 mx-auto rounded-lg">
-          <input type="text" placeholder="Enter Website URL" />
-          <div className="flex gap-10">
-            <input type="text" placeholder="Enter Username" />
-            <input type="text" placeholder="Enter Password" />
+
+        <div className="flex flex-col justify-center items-center h-[60vh] w-[60vw] border-2 border-[#efefef] mx-auto rounded-lg text-sm gap-4">
+          <input className="rounded-full border-2 border-[#cc4b85] relative w-[90%] p-3 py-1" type="text" placeholder="Enter Website URL" />
+          <div className="flex gap-10 w-[90%]">
+          <input className="rounded-full border-2 border-[#cc4b85] relative w-[90%] p-3 py-1" type="text" placeholder="Enter Username" />
+          <input className="rounded-full border-2 border-[#cc4b85] relative w-[90%] p-3 py-1" type="text" placeholder="Enter Password" />
           </div>
           <button className="bg-white">Add</button>
         </div>
       </div>
-
-      {/* <div className="flex flex-col h-[10vh]"></div> */}
-
-      {/* <div className=" mx-auto my-[30vh] max-w-2xl border-2">
-        <div className=" max-w-full flex flex-col border-2 border-red-300 gap-2">
-
-          <input
-            className="field bg-red-200"
-            type="text"
-            placeholder="Enter Website URL"
-          />
-          <div className="flex flex-row w-1/2 justify-between">
-            <input
-              className="bg-green-200"
-              type="text"
-              placeholder="Enter Username"
-            />
-            <input
-              className="bg-blue-200"
-              type="text"
-              placeholder="Enter Password"
-            />
-          </div>
-        </div>
-        <div className="buttons">
-          <button className="bg-white">Add</button>
-        </div>
-      
-      </div> */}
     </div>
   );
 };
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,10 +5,10 @@ const Navbar = () => {
   return (
     <div className="container flex flex-row justify-between items-center bg-gray-200 mx-auto max-w-full">
       <div className="logo flex flex-row justify-center items-center w-1/4 text-lg font-bold ">
-      <span className="text-[#010c29] text-4xl">&#91;</span>
-          <img className="w-14" src={logo} alt="" />
-          Pass<span className="text-[#cc4b85] mr-2 ml-">Codes</span>
-          <span className="text-[#010c29] text-4xl">&#93;</span>
+        <span className="text-[#010c29] text-4xl">&#91;</span>
+        <img className="w-14" src={logo} alt="" />
+        Pass<span className="text-[#cc4b85] mr-2 ml-">Codes</span>
+        <span className="text-[#010c29] text-4xl">&#93;</span>
       </div>
       <ul className="flex flex-row w-1/4 justify-evenly">
         <li>
