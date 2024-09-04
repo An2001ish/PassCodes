91869f6547a28bcc6a0c2ea5e252837a8cb6d035
Anish Baraily <An2001.baraily@gmail.com>
2024-09-04 21:27:10 +0545

    ui done

--- src/components/Manager.jsx
+++ src/components/Manager.jsx
@@ -1,10 +1,17 @@
-import React from "react";
+import React, { useRef } from "react";
 import logo from "./logo.png";
 
 const Manager = () => {
+  const ref = useRef()
+  const showPassword = () => {
+    ref.current.src.includes("icons/eye.png")
+      ? (ref.current.src = "icons/hidden.png")
+      : (ref.current.src = "icons/eye.png");
+  };
+
   return (
     <div>
-      <div class="absolute top-0 z-[-2] h-screen w-screen bg-[#000000] bg-[radial-gradient(#ffffff33_1px,#00091d_1px)] bg-[size:20px_20px]"></div>
+      <div className="absolute top-0 z-[-2] h-screen w-screen bg-[#000000] bg-[radial-gradient(#ffffff33_1px,#00091d_1px)] bg-[size:20px_20px]"></div>
 
       <div className="flex flex-col relative top-10 items-center">
         <div className="flex justify-center items-center absolute bottom-[158px] bg-[#000000] bg-[radial-gradient(#ffffff33_1px,#00091d_1px)] bg-[size:20px_20px] text-white text-base font-bold">
@@ -23,7 +30,7 @@ const Manager = () => {
             type="text"
             placeholder="Enter Website URL"
           />
-          <div className="flex gap-10 w-[90%]">
+          <div className="flex gap-10 w-[90%] relative">
             <input
               className="rounded-full border-2 border-[#cc4b85] relative w-[90%] p-3 py-0.5"
               type="text"
@@ -34,6 +41,18 @@ const Manager = () => {
               type="text"
               placeholder="Enter Password"
             />
+            <span
+              className="absolute top-0 right-2 cursor-pointer"
+              onClick={showPassword}
+            >
+              <img
+                className="p-1"
+                width={28}
+                src="icons/eye.png"
+                ref={ref}
+                alt=""
+              />
+            </span>
           </div>
 
           <button className="bg-[#cc4b85] rounded-lg w-40 flex items-center justify-center relative top-4 font-bold gap-2 hover:bg-[#e45595]">
